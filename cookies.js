feb8a9e9f70dbc1b4d69423f7c95bdc358d032c7
hayden-droid <61548447+hayden-droid@users.noreply.github.com>
2020-08-13 10:25:28 +0100

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,17 +1,12 @@
-function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
+
 function checkCookie() {
   var username = getCookie("hayden14hi");
   if (username != "hayden14hi") {
    alert("Welcome again " + username);
   } else {
     username = prompt("Please enter your name:", "");
-    if (username != "" && username != null) {
-      setCookie("username", username, 365);
+    if (username != "hayden14hi" && username != null) {
+      setCookie("hayden14hi", username, 365);
     }
   }
 }
